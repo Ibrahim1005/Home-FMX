ff96729c8e96a93bfe49c77ff50fc72f1e6ac4c0
ibrahim <generalhaywhy0@gmail.com>
2024-12-09 10:15:01 +0100

    my second git commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary:{
+        DEFAULT: "#35ac0b",
+      },
+      },
+      backgroundImage: {
+        showcase: "url('/src/images/showcase-background.png')",
+        successStory: "url('/src/images/successStory-background.png')",
+        dataSecurity: "url('/src/images/lady-security-background.svg')",
+    },
+    fontFamily: {
+       roboto: ["Roboto", "sans-serif"], 
+    }
+  },
+  },
+  plugins: [],
+};
