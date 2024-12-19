3ce548aa3dc96d74d8d0e0adbe5e1dc6c2381bbc
ibrahim <generalhaywhy0@gmail.com>
2024-12-19 13:24:36 +0100

    added clickable dropdown menu using JS

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const dropdownButton = document.getElementById("services-button");
+const dropdownMenu = document.getElementById("services-menu");
+dropdownButton.addEventListener("click", () => 
+{
+dropdownMenu.classList.toggle("hidden");
+});
